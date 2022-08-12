acbf3f122145078bb08b9f2d3a65b518c40e981f
sotopro <dasg.1993@gmail.com>
2022-08-12 11:57:09 -0500

    [feature] - add validation in auth fields & insert in database

--- app/src/main/java/com/example/animalplace/AuthActivity.java
+++ app/src/main/java/com/example/animalplace/AuthActivity.java
@@ -3,12 +3,58 @@ package com.example.animalplace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.animalplace.db.DbUsers;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AuthActivity extends AppCompatActivity {
+
+    TextInputEditText inputEmail, inputPassword;
+    Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        inputEmail = findViewById(R.id.editText_email);
+        inputPassword = findViewById(R.id.editText_password);
+        btnSignIn = findViewById(R.id.button_sign_in);
+        btnSignUp = findViewById(R.id.button_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(AuthActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    DbUsers dbUsers = new DbUsers(AuthActivity.this);
+                    boolean exists = dbUsers.isUserExists(email, password);
+                    if (exists) {
+                        Toast.makeText(AuthActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
+                    } else {
+                        long id = dbUsers.insertUser(email, password);
+
+                        if (id > 0) {
+                            Toast.makeText(AuthActivity.this, "User created", Toast.LENGTH_SHORT).show();
+                            cleanFields();
+                        } else {
+                            Toast.makeText(AuthActivity.this, "User not created", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void cleanFields() {
+        inputEmail.setText("");
+        inputPassword.setText("");
     }
 }
--- app/src/main/java/com/example/animalplace/db/DbUsers.java
+++ app/src/main/java/com/example/animalplace/db/DbUsers.java
@@ -2,15 +2,16 @@ package com.example.animalplace.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import androidx.annotation.Nullable;
 
-public class dbUsers extends DbHelper {
+public class DbUsers extends DbHelper {
 
     Context context;
 
-    public dbUsers(@Nullable Context context) {
+    public DbUsers(@Nullable Context context) {
         super(context);
         this.context = context;
     }
@@ -29,5 +30,21 @@ public class dbUsers extends DbHelper {
         }
 
         return id;
+    }
+
+    public boolean isUserExists(String email, String password) {
+        boolean exists = false;
+        try {
+            DbHelper dbHelper = new DbHelper(context);
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            String query = "SELECT * FROM " + TABLE_USER + " WHERE email = '" + email + "' AND password = '" + password + "'";
+            Cursor cursor = db.rawQuery(query, null);
+            if (cursor.moveToFirst()) {
+                exists = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return exists;
     }
 }
